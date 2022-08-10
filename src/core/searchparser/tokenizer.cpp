3e4686a7e69cf51203e6f57ae3784b416f705b53
realth000 <a598919335@gmail.com>
2022-08-11 01:13:47 +0800

    feat(core): Update search tokenizer
    Update search tokenizer.
    
    [skip ci]

--- src/core/searchparser/tokenizer.cpp
+++ src/core/searchparser/tokenizer.cpp
@@ -15,51 +15,109 @@ bool isMetadataKeyword(const QString& s) {
 
 Tokenizer::Tokenizer() {}
 
-TokenFormat Tokenizer::tokenize(const QString& rawString, bool* ok,
-                                QString* errString) {
+TokenList Tokenizer::tokenize(const QString& rawString, bool* ok,
+                              QString* errString) {
   if (ok == nullptr || errString == nullptr) {
     qDebug() << "tokenize error: null feedback not allowed in this step";
-    return TokenFormat{};
+    return TokenList{};
   }
-  TokenFormat f;
-  const QStringList wordsList = rawString.split(' ', QString::SkipEmptyParts);
-  if (wordsList.length() < 3) {
-    // If using an expression, at least have 3 words.
-    if (isKeyword(wordsList.last())) {
-      *ok = false;
-      *errString =
-          QString("tokenize error: token part not complete: %1").arg(rawString);
-      return TokenFormat{};
+  const int end = rawString.length();
+  TokenList tokenList;
+  for (int i = 0; i < end; i++) {
+    // Skip space.
+    if (rawString[i] == ' ') {
+      continue;
     }
-    f.text.append(wordsList);
-    *ok = true;
-    *errString = "";
-    return f;
-  }
-  if (!isMetadataKeyword(wordsList[0])) {
-    *ok = false;
-    *errString =
-        QString("tokenize error: invalid metadata keyword: %1").arg(rawString);
-    return TokenFormat{};
-  }
-  f.keyword = metadataKeywords[wordsList[0]];
-  if (!isKeyword(wordsList[1])) {
-    *ok = false;
-    *errString = QString("tokenize error: invalid keyword: %1").arg(rawString);
-    return TokenFormat{};
-  }
-  f.metadataKeyWord = wordsList[1];
-  for (int i = 2; i < wordsList.length(); i++) {
-    if (isKeyword(wordsList[i])) {
-      *ok = false;
-      *errString =
-          QString("tokenize error: unexpected keyword: %1").arg(rawString);
-      return TokenFormat{};
+    // Consider the word after '\\' as plain text.
+    if (rawString[i] == '\\') {
+      if (i == end - 1) {
+        *ok = false;
+        *errString = "unexpected extra '\\' at the end of raw string";
+        return {};
+      }
+      i++;
+      Token t;
+      t.type = TokenType::Word;
+      t.start = i;
+      while (i < end && rawString[i] != ' ') {
+        t.content.append(rawString[i]);
+        i++;
+      }
+      t.end = i - 1;
+      tokenList.append(t);
+      continue;
     }
-    f.text.append(wordsList[i]);
+    // Record '('.
+    if (rawString[i] == '(') {
+      Token t = Token(TokenType::LeftParentheses, i);
+      tokenList.append(t);
+      continue;
+    }
+    // Record ')'.
+    if (rawString[i] == ')') {
+      Token t = Token(TokenType::RightParentheses, i);
+      tokenList.append(t);
+      continue;
+    }
+
+    // Record keyword between %.
+    if (rawString[i] == '%') {
+      if (i == end - 1) {
+        *ok = false;
+        *errString = QString("unpaired '%' at end of raw string 1");
+        return {};
+      }
+      if (rawString[++i] == '%') {
+        *ok = false;
+        *errString = QString("empty quote in '%' at pos %1").arg(i);
+        return {};
+      }
+      Token t;
+      t.type = TokenType::Percent;
+      t.start = i;
+      while (i < end) {
+        if (rawString[i] == '%') {
+          if (i < end - 1 && rawString[i + 1] != ' ') {
+            *ok = false;
+            *errString = QString("unexpected '%' at pos %1").arg(i);
+            return {};
+          }
+          t.end = i - 1;
+          tokenList.append(t);
+          i++;
+          break;
+        }
+        if (rawString[i] == '\\' || rawString[i] == ')' ||
+            rawString[i] == ')') {
+          *ok = false;
+          *errString =
+              QString("unexpected '%1' at pos %2").arg(rawString[i], i);
+          return {};
+        }
+        if (i == end - 1) {
+          *ok = false;
+          *errString = QString("expect '%' at end of raw string");
+          return {};
+        }
+        t.content.append(rawString[i]);
+        i++;
+      }
+    }
+    // Record plain text.
+    Token t;
+    t.type = TokenType::Word;
+    t.start = i;
+    while (i < end && rawString[i] != ' ') {
+      t.content.append(rawString[i]);
+      i++;
+    }
+    t.end = i;
+    if (!t.content.isEmpty()) {
+      tokenList.append(t);
+    }
   }
   *ok = true;
   *errString = "";
-  return f;
+  return tokenList;
 }
 }  // namespace SearchParser
--- src/core/searchparser/tokenizer.h
+++ src/core/searchparser/tokenizer.h
@@ -1,21 +1,50 @@
 #ifndef MPAX_TOKENIZER_H
 #define MPAX_TOKENIZER_H
 
+#include <QtCore/QList>
+#include <QtCore/QPair>
 #include <QtCore/QString>
-#include <QtCore/QStringList>
 
 namespace SearchParser {
 
-struct TokenFormat {
-  QString keyword;
-  QString metadataKeyWord;
-  QStringList text;
+enum TokenType {
+  Word,
+  Space,
+  Percent,
+  LeftParentheses,
+  RightParentheses,
+  Escape,
+  Unknown
 };
+
+class Token {
+ public:
+  Token() {
+    type = Unknown;
+    content = "";
+    start = -1;
+    end = -1;
+  };
+  Token(TokenType t, int pos) {
+    type = t;
+    start = pos;
+    end = pos;
+  };
+  int length() const {
+    return (start < 0 || end < 0 || end < start) ? -1 : end - start + 1;
+  };
+  TokenType type;
+  QString content;
+  int start;
+  int end;
+};
+
+typedef QList<Token> TokenList;
 
 class Tokenizer {
  public:
   explicit Tokenizer();
-  TokenFormat tokenize(const QString& rawString, bool* ok, QString* errString);
+  TokenList tokenize(const QString& rawString, bool* ok, QString* errString);
 
  private:
   QString m_rawString;
