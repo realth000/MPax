adf8ddbc3ef8252c2b54f0fe704f5dd916314de4
realth000 <a598919335@gmail.com>
2022-09-03 12:51:23 +0800

    feat(gui): Search dialog apply header config
    Search dialog apply header config.

--- src/gui/playlistsearchdialog.cpp
+++ src/gui/playlistsearchdialog.cpp
@@ -2,6 +2,7 @@
 
 #include <QtCore/QDebug>
 
+#include "config/appconfig.h"
 #include "ui_playlistsearchdialog.h"
 #include "util/cssloader.h"
 
@@ -12,7 +13,8 @@ PlaylistSearchDialog::PlaylistSearchDialog(QWidget *parent)
       m_tableViewWidthRadio(QList<qreal>{0.5, 0.2, 0.3}),
       m_rowCount(-1),
       m_rowPos(-1),
-      m_tableViewContextMenu(initTableViewContextMenu()) {
+      m_tableViewContextMenu(initTableViewContextMenu()),
+      m_header(PLModel::PlaylistModelHeader::getInstance()) {
   ui->setupUi(this);
   this->setWindowTitle(tr("Search in playlist"));
   ui->searchTableView->verticalHeader()->setHidden(true);
@@ -31,6 +33,13 @@ PlaylistSearchDialog::PlaylistSearchDialog(QWidget *parent)
   ui->searchLineEdit->setFocus();
   ui->searchTableView->setModel(m_model);
   ui->searchTableView->setSortingEnabled(false);
+
+  auto headerVector = m_header->headerVector();
+  for (auto &h : headerVector) {
+    if (0 <= h.index && h.index < m_header->headerCount()) {
+      ui->searchTableView->setColumnWidth(h.index, h.width);
+    }
+  }
 
   connect(ui->searchLineEdit, &QLineEdit::textEdited, this,
           &PlaylistSearchDialog::updateSearchFilter);
@@ -51,6 +60,24 @@ PlaylistSearchDialog::~PlaylistSearchDialog() {}
 
 void PlaylistSearchDialog::setModel(QAbstractItemModel *model) {
   m_model->setSourceModel(model);
+  const QString sortHeader = Config::AppConfig::getInstance()
+                                 ->config(CONFIG_PLAYLIST_SORT_HEADER)
+                                 .value.toString();
+  const Qt::SortOrder sortOrder =
+      static_cast<Qt::SortOrder>(Config::AppConfig::getInstance()
+                                     ->config(CONFIG_PLAYLIST_SORT_ORDER)
+                                     .value.toInt());
+  for (int i = 0; i < m_header->headerCount(); i++) {
+    if (m_header->usedHeader(i) == sortHeader) {
+      ui->searchTableView->horizontalHeader()->setSortIndicator(i, sortOrder);
+    }
+  }
+  auto headerVector = m_header->headerVector();
+  for (auto &h : headerVector) {
+    if (0 <= h.index && h.index < m_header->headerCount()) {
+      ui->searchTableView->setColumnWidth(h.index, h.width);
+    }
+  }
 }
 
 void PlaylistSearchDialog::keyPressEvent(QKeyEvent *event) {
