realth000/MPax

commit 23d4c8a2d2125dff3c463f783d9ba6143217df66

fix(*): Apply sorting policy in sql to indicator
* Use the sorting policy in sqlite when a sorting requested from

playlist table indicator.

Closes #5

src/model/playlistmodel.cpp
@@ -290,6 +290,23 @@ void PlaylistModel::reloadPlayContentInfo(PlayContent *content) {
                            AudioInfo::InfoOption::NoAlbumCover);
 }
 
+void PlaylistModel::reloadPlaylistWithOrder(const int &column,
+                                            Qt::SortOrder order) {
+  Playlist *playlist = new Playlist(m_listInfo, m_contentList);
+  if (!PlaylistSql::getInstance()->loadPlaylistWithOrder(
+          playlist, m_header->usedHeader(column), order)) {
+    qDebug() << "failed to reload playlist with order";
+    return;
+  }
+  beginResetModel();
+  m_contentList.clear();
+  for (auto t : *(playlist->content())) {
+    m_contentList.append(t);
+  }
+  endResetModel();
+  delete playlist;
+}
+
 void PlaylistModel::setHeader(const PlaylistModelHeader *header) {
   m_header = header;
 }

src/model/playlistmodel.h
@@ -64,6 +64,7 @@ class PlaylistModel : public QAbstractItemModel {
  public slots:
   void reloadPlayContentInfo();
   void reloadPlayContentInfo(PlayContent *content);
+  void reloadPlaylistWithOrder(const int &column, Qt::SortOrder order);
 
  signals:
   void reloadInfoStatusChanged(QString playlistName, bool finished, int count,

src/core/playlist.cpp
@@ -53,4 +53,9 @@ Playlist::~Playlist() {
 
 const PlayContentList* Playlist::content() const { return m_playContentList; }
 
+void Playlist::setContent(PlayContentList* list) {
+  delete m_playContentList;
+  m_playContentList = list;
+}
+
 const PlaylistInfo* Playlist::info() const { return m_playListInfo; }

src/core/playlist.h
@@ -35,6 +35,7 @@ class Playlist {
   virtual ~Playlist();
   const PlaylistInfo *info() const;
   const PlayContentList *content() const;
+  void setContent(PlayContentList *list);
 
  private:
   PlaylistInfo *m_playListInfo;

src/core/playlistsql.cpp
@@ -334,3 +334,52 @@ void PlaylistSql::tryCloseDatabase() {
     m_database.close();
   }
 }
+
+bool PlaylistSql::loadPlaylistWithOrder(Playlist* playlist,
+                                        const QString& columnName,
+                                        Qt::SortOrder order) {
+  if (!tryOpenDatabase()) {
+    qDebug() << "can not load playlist with order, database failed to open";
+    return false;
+  }
+  const QString playlistName = playlist->info()->info(PLAYLIST_INFO_NAME);
+  if (playlistName.isEmpty()) {
+    qDebug() << "can not load playlist with order, empty playlist name";
+    return false;
+  }
+  PlayContentList* list = new PlayContentList;
+  QString tableName;
+  bool ok;
+  QSqlQuery q(m_database);
+  const QString sqlColumnName = m_titleMap[columnName];
+  if (sqlColumnName.isEmpty()) {
+    goto exit;
+  }
+  for (const auto& p : m_nameVector) {
+    if (p.second == playlistName) {
+      tableName = p.first;
+      break;
+    }
+  }
+  ok = q.exec(QString("SELECT * FROM %1 ORDER BY %2 %3")
+                  .arg(tableName, sqlColumnName,
+                       order == Qt::DescendingOrder ? "DESC" : "ASC"));
+  if (!ok) {
+    qDebug() << "can not load playlist with order" << playlistName << ":"
+             << q.lastError();
+    goto exit;
+  }
+  while (q.next()) {
+    PlayContent* playContent = new PlayContent(q.value("path").toString());
+    playContent->title = q.value("title").toString();
+    playContent->artist = q.value("artist").toString();
+    playContent->albumTitle = q.value("album_title").toString();
+    list->append(playContent);
+  }
+  playlist->setContent(list);
+  return true;
+
+exit:
+  tryCloseDatabase();
+  return false;
+}

src/core/playlistsql.h
@@ -20,6 +20,8 @@ class PlaylistSql : public QObject {
   void removePlaylist(const int& index);
   void updatePlaylist(const int& index, const Playlist& playlist);
   QList<Playlist> loadPlaylist();
+  bool loadPlaylistWithOrder(Playlist* playlist, const QString& columnName,
+                             Qt::SortOrder order);
 
  private:
   QSqlDatabase m_database;

src/model/playlistfiltermodel.cpp
@@ -1,5 +1,9 @@
 #include "playlistfiltermodel.h"
 
+#include <QtCore/QDebug>
+
+#include "model/playlistmodel.h"
+
 PlaylistFilterModel::PlaylistFilterModel(QObject *parent)
     : QSortFilterProxyModel(parent) {}
 
@@ -25,6 +29,19 @@ bool PlaylistFilterModel::lessThan(const QModelIndex &left,
   return QSortFilterProxyModel::lessThan(left, right);
 }
 
+void PlaylistFilterModel::sort(int column, Qt::SortOrder order) {
+  /**
+   * Consider the sorting policy in sql as the true order.
+   * For example, in ascending order:
+   * 1. Alphabets [a-z]
+   * 2. Characters in other languages.
+   */
+  emit sortFromSql(column, order);
+#if 0
+  QSortFilterProxyModel::sort(column, order);
+#endif
+}
+
 QModelIndex PlaylistFilterModel::sourceIndex(
     const QModelIndex &proxyIndex) const {
   return mapToSource(proxyIndex);
@@ -36,6 +53,19 @@ QModelIndex PlaylistFilterModel::seekSourceRow(const QModelIndex &proxyIndex,
     return mapToSource(index(0, 0));
   }
   return mapToSource(proxyIndex.siblingAtRow(nIndex));
+}
+
+void PlaylistFilterModel::setSourceModel(QAbstractItemModel *sourceModel) {
+  PlaylistModel *sourcePlaylistModel =
+      reinterpret_cast<PlaylistModel *>(sourceModel);
+  if (sourcePlaylistModel != nullptr) {
+    if (sourceModel != nullptr) {
+      disconnect(this, &PlaylistFilterModel::sortFromSql, sourceModel, nullptr);
+    }
+    connect(this, &PlaylistFilterModel::sortFromSql, sourcePlaylistModel,
+            &PlaylistModel::reloadPlaylistWithOrder);
+  }
+  QSortFilterProxyModel::setSourceModel(sourceModel);
 }
 
 QModelIndex PlaylistFilterModel::fromSourceIndex(

src/model/playlistfiltermodel.h
@@ -11,12 +11,18 @@ class PlaylistFilterModel : public QSortFilterProxyModel {
   QModelIndex fromSourceIndex(const QModelIndex &sourceIndex) const;
   QModelIndex seekSourceRow(const QModelIndex &proxyIndex,
                             const int &offset) const;
+  virtual void setSourceModel(QAbstractItemModel *sourceModel) override;
+
+ signals:
+  void sortFromSql(int column, Qt::SortOrder order);
 
  protected:
   bool filterAcceptsRow(int sourceRow,
                         const QModelIndex &sourceParent) const override;
   bool lessThan(const QModelIndex &left,
                 const QModelIndex &right) const override;
+  virtual void sort(int column,
+                    Qt::SortOrder order = Qt::AscendingOrder) override;
 };
 
 #endif  // MPAX_PLAYLISTFILTERMODEL_H

commit b401ffe34573054f019dd0d47f077289e66667ff

fix(*): Fix crash when update sort and play next
Fix crash when update sort and play next:
* This issue probably introduced in 23d4c8a, the sort caused a rebuild
  on playlist, which let the current playlist content pointer expired.
  In this commit, we add an update after the sort, including all info
  related to the old playlist.

src/gui/listtabwidget.cpp
@@ -38,6 +38,8 @@ void ListTabWidget::InitConnections() {
           &ListTabWidget::openListViewContextMenu);
   connect(m_listTabModel, &ListTabModel::reloadInfoStatusChanged, this,
           &ListTabWidget::reloadInfoStatusChanged);
+  connect(m_listTabModel, &ListTabModel::currentPlayContentUpdated, this,
+          &ListTabWidget::currentPlayContentUpdated);
 }
 
 void ListTabWidget::updateCurrentPlaylist(const QModelIndex &index) {

src/gui/listtabwidget.h
@@ -22,6 +22,7 @@ class ListTabWidget : public QWidget {
   void currentPlaylistIndexChanged(const int &index);
   void reloadInfoStatusChanged(QString playlistName, bool finished, int count,
                                qint64 time);
+  void currentPlayContentUpdated(PlayContentPos pos);
 
  public slots:
   void addPlaylist(PlaylistModel *playlistModel);

src/gui/mainui.cpp
@@ -149,8 +149,13 @@ void MainUI::InitConnections() {
           ui->playlistWidget, &PlaylistWidget::removeContents);
   connect(m_searchDialog, &PlaylistSearchDialog::openFileInDirTriggered,
           ui->playlistWidget, &PlaylistWidget::openFileInDir);
-  connect(ui->playControlWidget, &PlayControlWidget::currentPlayContentChanged,
-          this, &MainUI::handleCurrentPlayContentChanged);
+  connect(
+      ui->listTabWidget, &ListTabWidget::currentPlayContentUpdated, this,
+      QOverload<PlayContentPos>::of(&MainUI::handleCurrentPlayContentChanged));
+  connect(
+      ui->playControlWidget, &PlayControlWidget::currentPlayContentChanged,
+      this,
+      QOverload<const QUrl &>::of(&MainUI::handleCurrentPlayContentChanged));
   connect(this, &MainUI::scrollToContent, ui->playlistWidget,
           &PlaylistWidget::scrollToContent);
 
@@ -464,6 +469,12 @@ void MainUI::handleCurrentPlayContentChanged(const QUrl &contentUrl) {
     return;
   }
   emit scrollToContent(contentUrl.toLocalFile());
+}
+
+void MainUI::handleCurrentPlayContentChanged(PlayContentPos pos) {
+  if (pos.index > 0 && pos.content != nullptr) {
+    emit scrollToContent(pos.content->contentPath);
+  }
 }
 
 void MainUI::updateMainWindowVisible(bool toShow) {

src/gui/mainui.h
@@ -66,6 +66,7 @@ class MainUI : public QMainWindow {
   void updateReloadInfoStatus(const QString &playlistName, bool finished,
                               int count, qint64 time);
   void handleCurrentPlayContentChanged(const QUrl &contentUrl);
+  void handleCurrentPlayContentChanged(PlayContentPos pos);
   void updateMainWindowVisible(bool toShow);
   void exitApp();
   void restartApp();

src/model/listtabmodel.cpp
@@ -50,6 +50,8 @@ void ListTabModel::addPlaylist(PlaylistModel *playlistModel) {
   }
   connect(playlistModel, &PlaylistModel::reloadInfoStatusChanged, this,
           &ListTabModel::reloadInfoStatusChanged);
+  connect(playlistModel, &PlaylistModel::currentPlayContentUpdated, this,
+          &ListTabModel::currentPlayContentUpdated);
 #if 0
   QMetaObject::invokeMethod(playlistModel, "reloadPlayContentInfo",
                             Qt::QueuedConnection);

src/model/listtabmodel.h
@@ -25,6 +25,7 @@ class ListTabModel : public QStringListModel {
   void dataChanged();
   void reloadInfoStatusChanged(QString playlistName, bool finished, int count,
                                qint64 time);
+  void currentPlayContentUpdated(PlayContentPos pos);
   void reloadPlaylist();
 
  public slots:

src/model/playlistmodel.cpp
@@ -124,8 +124,11 @@ void PlaylistModel::setPlaylistName(const QString &name) {
 QString PlaylistModel::playlistName() const { return m_playlistName; }
 
 PlayContentPos PlaylistModel::currentPlayContent() const {
-  return PlayContentPos{m_playlist->content().indexOf(m_currentPlayContent),
-                        m_currentPlayContent};
+  int i = m_playlist->content().indexOf(m_currentPlayContent);
+  if (i < 0) {
+    return PlayContentPos{-1, nullptr};
+  }
+  return PlayContentPos{i, m_currentPlayContent};
 }
 
 void PlaylistModel::setCurrentPlayContent(const int &index) {
@@ -250,6 +253,9 @@ void PlaylistModel::reloadPlaylistWithOrder(const int &column,
     return;
   }
   endResetModel();
+  // Playlist will be rebuilt here, everything about this playlist that requires
+  // update must be done.
+  updatePlaylistState();
 }
 
 const QModelIndex PlaylistModel::find(const QString &contentPath) const {
@@ -269,4 +275,16 @@ void PlaylistModel::setUsedHeader(const QString &header, bool used) {
   beginResetModel();
   PLModel::PlaylistModelHeader::getInstance()->setUsedHeader(header, used);
   endResetModel();
+}
+
+void PlaylistModel::updatePlaylistState() {
+  PlayContentList playContentList = m_playlist->content();
+  for (int i = 0; i < playContentList.length(); i++) {
+    if (m_currentPlayContent != nullptr &&
+        playContentList[i]->contentPath == m_currentPlayContent->contentPath) {
+      m_currentPlayContent = playContentList[i];
+      emit currentPlayContentUpdated(PlayContentPos{i, m_currentPlayContent});
+      break;
+    }
+  }
 }

src/model/playlistmodel.h
@@ -59,6 +59,7 @@ class PlaylistModel : public QAbstractItemModel {
  signals:
   void reloadInfoStatusChanged(QString playlistName, bool finished, int count,
                                qint64 time);
+  void currentPlayContentUpdated(PlayContentPos pos);
 
  private:
   QString m_playlistName;
@@ -69,5 +70,7 @@ class PlaylistModel : public QAbstractItemModel {
   PlayContent *m_currentPlayContent;
   // FIXME: Duplicate with PlaylistModelHeader::m_headerTrans
   QMap<QString, QString> m_headerTrans;
+
+  void updatePlaylistState();
 };
 #endif  // PLAYLISTMODEL_H